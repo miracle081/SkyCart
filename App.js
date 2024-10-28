80add677e2b4488797ff6d96a2460cba3f48e8bb
Miracle Obafemi <miracleobafemi09@gmail.com>
2024-10-28 16:25:32 +0100

    New changes

--- App.js
+++ App.js
@@ -1,13 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
-import { HomeScreen } from './Framework/Screen/HomeScreen';
-import { Intro } from './Framework/Screen/Intro';
-import { Profile } from './Framework/Screen/Profile';
+import { useEffect, useState, useCallback } from "react";
+import { LogBox, View } from "react-native";
+import { Pacifico_400Regular } from "@expo-google-fonts/pacifico";
+import {
+  Montserrat_100Thin, Montserrat_200ExtraLight, Montserrat_300Light, Montserrat_400Regular,
+  Montserrat_500Medium, Montserrat_600SemiBold, Montserrat_700Bold, Montserrat_800ExtraBold, Montserrat_900Black
+} from "@expo-google-fonts/montserrat"
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import { StackNavigator } from "./Framework/Navigation/StackNavigator";
+
+LogBox.ignoreLogs(["ViewPropTypes will be removed from React Native, along with all other PropTypes. We recommend that you migrate away from PropTypes and switch to a type system like TypeScript. If you need to continue using ViewPropTypes, migrate to the 'deprecated-react-native-prop-types' package."])
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({ Pacifico_400Regular });
+        await Font.loadAsync({ Montserrat_100Thin });
+        await Font.loadAsync({ Montserrat_200ExtraLight });
+        await Font.loadAsync({ Montserrat_300Light });
+        await Font.loadAsync({ Montserrat_400Regular });
+        await Font.loadAsync({ Montserrat_500Medium });
+        await Font.loadAsync({ Montserrat_600SemiBold });
+        await Font.loadAsync({ Montserrat_700Bold });
+        await Font.loadAsync({ Montserrat_800ExtraBold });
+        await Font.loadAsync({ Montserrat_900Black });
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
   return (
     <View style={{ flex: 1 }}>
-      <Profile />
+      <StackNavigator />
     </View>
   );
 }
