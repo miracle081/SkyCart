92209f3ef6ad6a6059b31452e1934c0fabc762cf
Miracle Obafemi <miracleobafemi09@gmail.com>
2024-10-29 14:20:25 +0100

    New changes

--- Framework/Screens/HomeScreen.js
+++ Framework/Screens/HomeScreen.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Profile } from './Profile';
+import { Ionicons } from '@expo/vector-icons';
 
 function Home() {
   return (
@@ -17,7 +18,38 @@ const Tab = createBottomTabNavigator();
 
 export function HomeScreen() {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false, }}>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color }) => {
+          let iconName;
+          let size;
+          if (route.name === 'Home') {
+            size = focused ? 35 : 23
+            iconName = focused ? 'home' : 'home-outline';
+          }
+          if (route.name === 'Assets') {
+            size = focused ? 35 : 23
+            iconName = focused ? 'diamond' : 'diamond-outline';
+          }
+          if (route.name === 'PeerToPeer') {
+            size = focused ? 35 : 23
+            iconName = focused ? 'swap-horizontal' : 'swap-horizontal-outline';
+          }
+          if (route.name === 'MyShareUnit') {
+            size = focused ? 35 : 23
+            iconName = focused ? 'bar-chart' : 'bar-chart-outline';
+          }
+          else if (route.name === 'Profile') {
+            size = focused ? 35 : 23
+            iconName = focused ? 'person' : 'person-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: Theme.colors.primary,
+        tabBarInactiveTintColor: Theme.colors.blueMedium,
+        headerShown: false,
+      })}>
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
