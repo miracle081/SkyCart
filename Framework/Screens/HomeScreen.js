c7a64ea17638b863072f51a68ae2709412d4825d
Miracle Obafemi <miracleobafemi09@gmail.com>
2024-10-29 17:03:55 +0100

    New changes

--- Framework/Screens/HomeScreen.js
+++ Framework/Screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useContext } from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Profile } from './Profile';
 import { Ionicons } from '@expo/vector-icons';
+import { AppContext } from '../Components/globalVariables';
 
 function Home() {
+  const { } = useContext(AppContext)
   return (
     <View>
       <Text>HomeScreen</Text>
